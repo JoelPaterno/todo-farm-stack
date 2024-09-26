78b864fb8250089d0e71852b70ccddb44b5df3ef
joelpaterno <joelpaterno1@gmail.com>
2024-09-26 16:54:13 +1000

    fix commit -2

--- backend/src/dal.py
+++ backend/src/dal.py
@@ -14,9 +14,9 @@ class ListSummary(BaseModel):
     @staticmethod
     def from_doc(doc) -> "ListSummary":
         return ListSummary(
-            id=str(doc["_id"]),
-            name=doc["name"],
-            item_count=doc["item_count"]
+            id = str(doc["_id"]),
+            name = doc["name"],
+            item_count = doc["item_count"]
         ) 
 class ToDoListItem(BaseModel):
     id: str
@@ -26,5 +26,39 @@ class ToDoListItem(BaseModel):
     @staticmethod
     def from_doc(item) -> "ToDoListItem":
         return ToDoListItem(
-            
+            id = item["id"],
+            label=item["label"],
+            checked = item["checked"],
         )
+class ToDoList(BaseModel):
+    id: str
+    name: str
+    items: list[ToDoListItem]
+
+    @staticmethod
+    def from_doc(doc) -> "ToDoList":
+        return ToDoList(
+            id = str(doc["_id"]),
+            name = doc["name"],
+            items = [ToDoListItem.from_doc(item) for item in doc["items"]],
+        )
+class ToDoDal:
+    def __init__(self, todo_collection: AsyncIOMotorCollection):
+        self.todo_collection = todo_collection
+    async def list_todo_lists(self,session=None):
+        async for doc in self._todo_collection.find(
+            {},
+            projection={
+                "name": 1,
+                "item_count": {"$size": "$items"},
+            },
+            sort={"name": 1},
+            session=session,
+        ):
+            yield ListSummary.from_doc(doc)
+    async def create_todo_list(self, name:str, session=None):
+        response = await self._todo_collection.insert_one(
+            {"name": name, "items": []},
+            session=session,
+        )
+        return str(response.inserted_id)
