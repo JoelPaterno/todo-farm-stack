60cc3c81a7dbd21a06aa587e4ce05f35031e851b
joelpaterno <joelpaterno1@gmail.com>
2024-09-27 12:45:59 +1000

    setting up backend

--- backend/src/dal.py
+++ backend/src/dal.py
@@ -57,8 +57,56 @@ class ToDoDal:
         ):
             yield ListSummary.from_doc(doc)
     async def create_todo_list(self, name:str, session=None):
+
         response = await self._todo_collection.insert_one(
             {"name": name, "items": []},
             session=session,
         )
         return str(response.inserted_id)
+    async def get_todo_list(self, id:str | ObjectId, session=None) -> ToDoList:
+        doc = await self._todo_collection.find_one(
+            {"_id": ObjectId(id)},
+            session=session,
+        )
+        return ToDoList.from_doc(doc)
+    async def delete_todo_list(self, id:str | ObjectId, session=None) -> bool:
+        response = await self._todo_collection.delete_one(
+            {"_id": ObjectId(id)},
+            session=session
+        )
+        return response.deleted_count == 1
+    async def create_item(self, id:str | ObjectId, label:str, session=None) -> ToDoList | None:
+        result = await self._todo_collection.find_one_and_update(
+            {"_id": ObjectId(id)},
+            {
+                "$push": {
+                    "items" : {
+                        "id": uuid4().hex,
+                        "label": label,
+                        "checked": False,
+                    }
+                }
+            },
+            session=session,
+            return_document=ReturnDocument.AFTER,
+        )
+        if result:
+            return ToDoList.from_doc(result)
+    async def set_checked_state(self, doc_id:str | ObjectId, item_id:str, checked_state: bool, session=None) -> ToDoList | None:
+        result = await self._todo_collection.find_one_and_update(
+          {"_id": ObjectId(doc_id), "items.id": item_id},
+          {"$set": {"items.$.checked": checked_state}},
+          session=session,
+          return_document=ReturnDocument.AFTER,
+        )
+        if result:
+            return ToDoList.from_doc(result)
+    async def delete_item(self, doc_id: str | ObjectId, item_id: str, session=None) -> ToDoList | None:
+        result = await self._todo_collection.find_one_and_update(
+            {"_id": ObjectId(doc_id)},
+            {"$pull": {"items": {"id": item_id}}},
+            session=session,
+            return_document=ReturnDocument.AFTER,
+        )
+        if result:
+            return ToDoList.from_doc(result)
